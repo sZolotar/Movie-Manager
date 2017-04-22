fc5e118c4c206d2d32902dfe9f32c2906ac05bb2
Sergii Zolotar <sergeypf@gmail.com>
2017-04-23 00:08:48 +0300

    change movie controller type from controller to RestController (return JSON) and update pom.xml

--- src/main/java/com/zolotar/project/controller/MovieController.java
+++ src/main/java/com/zolotar/project/controller/MovieController.java
@@ -1,16 +1,24 @@
 package com.zolotar.project.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import com.zolotar.project.entity.Movie;
+import com.zolotar.project.service.MovieService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.inject.Inject;
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
 public class MovieController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(Model model){
-        model.addAttribute("message", "Hello MVC");
-        return "hello";
+    @Inject
+    MovieService movieService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Movie> getMovies() {
+        return movieService.findAll();
     }
+
 }
